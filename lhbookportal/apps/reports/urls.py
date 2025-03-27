5c4fde69ae31c8cb0dc3bd8df7d4836a78cbc3e8
Rahul <567rahulm567@gmail.com>
2025-03-27 12:46:16 +0530

    fixed download bill bug

--- lhbookportal/apps/reports/urls.py
+++ lhbookportal/apps/reports/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('download/bill/<int:booking_id>/', DownloadBillPDF.as_view(), name='download bill'),
-    path('download/bookings/<str:date>/', download_daily_schedule_csv, name = 'download_csv'),
+    path('download/schedule/<str:date>/', download_daily_schedule_csv, name = 'download_csv'),
     # path(),
 ]
